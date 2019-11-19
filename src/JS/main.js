07679203793fdc9dfc4969b3ad8a432f9a0141b1
NathaliBV <nathali.b.v@hotmail.com>
2019-11-19 17:55:19 -0600

    Se trabajó en la maquetación de las pantallas

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,15 +1,42 @@
-/**
- * import POKEMON from './data/pokemon/pokemon.js'
- * import LoL from './data/lol/lol.js'
- * import POTTER from './data/potter/potter.js'
- */
+window.onload = function() {
+    renderPokemon(pokemons);
+};
 
-import { example } from './data.js';
+function renderPokemon(anyArray) {
+    let pokemonsDiv = document.getElementById("pokemons-caja");
+    pokemonsDiv.innerHTML = `
+    ${anyArray.map((pokemon) => `
+    <div class="pokemon-item">
+        <img src="${pokemon["img"]}" class="pokemon-img" />
+        <div class="text-name">
+        <h3 class="pokemon-name">${pokemon["name"]}</h3>
+        </div>
+        <div class="text-number">
+        <p>${pokemon["num"]}</p>
+        </div>
+    </div>
+  `).join("")}
+      `
+  }
+  
+  function getName(event) {
+    event.preventDefault();
+    var name = document.getElementById("poke-name").value;
+    var pokemon = pokemons.find(element => {
+        return element.name == name;
+    })
+    renderPokemon([pokemon])
+        /*var elemImg = document.createElement("img");
+        elemImg.src = pokemon.img
+        var resultsDiv = document.getElementById("pokemons-caja");
+        resultsDiv.appendChild(elemImg);
+        */
+}
 
-console.log(example);
-
-/*
- * console.log(POKEMON);
- * console.log(LoL);
- * console.log(POTTER)
-*/
+function getType(sel) {
+    var type = sel.options[sel.selectedIndex].text
+    var pokemonType = pokemons.filter(element => {
+        return element.type == type;
+    })
+    renderPokemon(pokemonType)
+}
