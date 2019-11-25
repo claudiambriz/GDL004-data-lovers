49ff71072f2aa2620093b509942238c90be998ce
Claudia Ambriz <claudiambrizgarcia@gmail.com>
2019-11-25 08:26:53 -0600

    se agregó función Evolución

--- src/JS/main.js
+++ src/JS/main.js
@@ -53,4 +53,35 @@ function getType(sel) {
     renderPokemon(pokemonType)
 }
 
+// Función añadida por Claudia Ambriz para buscar 
+// las evolciones de un pokemon en base a su nombre
+function getEvolution() {
+
+    let name = document.getElementById("poke-name").value;
+    
+    // 1.- Encontramos el pokemon que estamos buscando
+    let pokemon = pokemons.find(element => {
+        return element.name.includes(name);    
+    })
+    
+    let pokemon_evolutions = []; // Nuevo arreglo para guardar los pokemons de las evoluciones
+  
+    // 2.- Nos traemos la lista de evoluciones desde el pokemon seleccionado
+    // generando un nuevo objeto pokemon por cada evolución (forEach)
+    pokemon.next_evolution.forEach(element => {
+        evolution_name = element.name;  // Guardamos el nombre de la evolución en esta variable
+        
+        // 2.1.- encontramos el pokemon en base al nombre de la evolución 
+        var pokemon_evolution = pokemons.find(element => {
+            return element.name.includes(evolution_name);
+        })
+        // alert("Adding evolution to new array pokemon_evolutions: " + pokemon_evolution.name)
+        
+        //3.- agregamos el pokemon encontrado a la lista de evoluciones pokemon_evolutions
+        pokemon_evolutions.push(pokemon_evolution);
+    });
+
+    //pintamos en el HTML (pantalla) la lista de los pokemones evolución del pokemon que buscamos
+    renderPokemon(pokemon_evolutions)
+}
 //onchange
